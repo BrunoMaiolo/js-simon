04ab6075a9064d48ece384926e42aa691083a0e0
BrunoMaiolo <brunosj@hotmail.it>
2026-01-14 15:48:40 +0100

    logica per geneare 5 numeri casuali

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -6,3 +6,11 @@ const messageEl = document.getElementById('message');
 
 //generare  5 numeri casuali
 const randomNumbers = [];
+while (randomNumbers.length < 5) {
+  const number = Math.floor(Math.random() * 50) + 1;
+
+  if (!randomNumbers.includes(number)) {
+    randomNumbers.push(number);
+  }
+}
+
