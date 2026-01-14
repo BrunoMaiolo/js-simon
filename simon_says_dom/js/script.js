dc38e30bb04e139ea8af6651d5a166079261db8f
BrunoMaiolo <brunosj@hotmail.it>
2026-01-14 15:34:05 +0100

    prendo elemento message dal dom

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,3 +1,4 @@
 const countdownEl = document.getElementById('countdown');
 const numbersList = document.getElementById('numbers-list');
 const answersForm = document.getElementById('answers-form');
+const messageEl = document.getElementById('message');
