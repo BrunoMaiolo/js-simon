2bed8776b39eb4f04f2dcbc574d9377163e8bcb4
BrunoMaiolo <brunosj@hotmail.it>
2026-01-14 15:36:02 +0100

    generare 5 numeri casuali

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,4 +1,8 @@
+//elementi dal dom//
 const countdownEl = document.getElementById('countdown');
 const numbersList = document.getElementById('numbers-list');
 const answersForm = document.getElementById('answers-form');
 const messageEl = document.getElementById('message');
+
+//generare  5 numeri casuali
+const randomNumbers = [];
